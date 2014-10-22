6636a4a20f9da98d91c22c6cc17e67bd17bd77be
larsac07 <larsac07@gmail.com>
2014-10-22 21:25:23 +0200

    Added some comments. I HELPED!

--- geneticAlgorith/src/no/uib/mof077/shortbytes/neural/XORNetwork.java
+++ geneticAlgorith/src/no/uib/mof077/shortbytes/neural/XORNetwork.java
@@ -11,26 +11,35 @@ public class XORNetwork {
 	ArrayList<Connection> connections = new ArrayList<>();
 
 	public XORNetwork() {
+		
+		// Create two input nodes with a 0 value and 1 threshold
 		Node input1 = new Node(0, 1);
 		Node input2 = new Node(0, 1);
+		// Add input nodes to inputList
 		ArrayList<Node> inputList = new ArrayList<>();
 		inputList.add(input1);
 		inputList.add(input2);
 
+		// Create two hidden nodes with a 0 value and 1 threshold
 		Node hidden1 = new Node(0, 1);
 		Node hidden2 = new Node(0, 1);
+		// Add hidden nodes to hiddenList
 		ArrayList<Node> hiddenList = new ArrayList<>();
 		hiddenList.add(hidden1);
 		hiddenList.add(hidden2);
 
+		// Create one output node with a 0 value and 1 threshold
 		Node output1 = new Node(0, 1);
+		// Add output node to outputList
 		ArrayList<Node> outputList = new ArrayList<>();
 		outputList.add(output1);
 
-		inputLayer = new NodeLayer(inputList);
-		hiddenLayer = new NodeLayer(hiddenList);
-		outputLayer = new NodeLayer(outputList);
+		// Create NodeLayers from input-, hidden- and outputLayer
+		this.inputLayer = new NodeLayer(inputList);
+		this.hiddenLayer = new NodeLayer(hiddenList);
+		this.outputLayer = new NodeLayer(outputList);
 
+		// Create Connections between nodes
 		addConnections(
 				new Connection(input1, hidden1, 1),
 				new Connection(input1, hidden2, -1),
@@ -41,16 +50,20 @@ public class XORNetwork {
 				);
 	}
 
-	public void addConnections(Connection... link1) {
+	/**
+	 * Helper method to add connections to the connection list
+	 * @param link1
+	 */
+	private void addConnections(Connection... link1) {
 		for(Connection c : link1) {
-			connections.add(c);
+			this.connections.add(c);
 		}
 	}
 
 	public void sendSignals() {
 		int count = 1;
 
-		for(Connection connection : connections) {
+		for(Connection connection : this.connections) {
 			if(connection.getFrom().getValue() >= connection.getFrom().getThreshold()) {
 				System.out.println("sending through link " + count +":" + connection.getFrom().getValue()*connection.getWeight());
 				connection.getTo().setValue(connection.getTo().getValue() + (connection.getFrom().getValue() * connection.getWeight()));
@@ -72,7 +85,7 @@ public class XORNetwork {
 	public static void main(String[] args) {
 		XORNetwork network = new XORNetwork();
 
-		network.inputLayer.nodeList.get(0).setValue(0);
+		network.inputLayer.nodeList.get(0).setValue(1);
 		network.inputLayer.nodeList.get(1).setValue(0);
 
 		network.sendSignals();
